ca91d045ee43cf7ca62afbd9ae58242df8c17619
Karol M <KarolMMM01@gmail.com>
2021-12-29 02:52:43 +0100

    Simple counting work

--- app.js
+++ app.js
@@ -1,60 +1,68 @@
-const numButtons = document.querySelectorAll('.number')
-const displayDiv = document.querySelector('.display')
-const opeButtons = document.querySelectorAll('.operator')
-const eqButton = document.querySelector('.equal')
-let currentNumber = [];
-let passToCount = []
-let operator = false;
+const numButtons = document.querySelectorAll(".number");
+const displayDiv = document.querySelector(".display");
+const opeButtons = document.querySelectorAll(".operator");
+const eqButton = document.querySelector(".equal");
+const inputNumber = [];
+const passToCount = [];
+let lastOperator;
+let total = 0;
 
-
-function handleNumbers(e){
-    currentNumber.push(e.target.innerText)
-    const intNumber = parseInt(currentNumber.join(''))
-    displayDiv.innerText = (intNumber);
-    // console.log(currentNumber);
-    // console.log(intNumber);
-};
-
-
-function handleOperation(e){
-    operator = true
-    let total = 0
-    let lastOperator = e.target.innerHTML
-    // if(lastOperator === '='){
-    //     return total
-    // }
-    const operatorOptions = {
-        '+': function(a,b) {return a+b},
-        '-': function(a,b) {return a-b},
-        '*': function(a,b) {return a*b},
-        '/': function(a,b) {return a/b},
-    }
-    console.log(e.target.innerHTML);
-    if(currentNumber.length === 0){
-        return
-    }
-    const workingNum = parseInt(currentNumber.join(''))
-    // console.log(workingNum);
-    passToCount.push(workingNum)
-    console.log(passToCount);
-    currentNumber.length = 0;
-    if(passToCount.length === 2){
-         passToCount.reduce(function(prevVal, currentVal){
-            return total = operatorOptions[lastOperator](prevVal,currentVal);
-        });
-        passToCount.length = 0
-        passToCount.push(total);
-    }
-    console.log(total);
-    return total
+function displayValue(e) {
+  inputNumber.push(e.target.innerText);
+  const numberToDisplay = inputNumber.join("");
+  displayDiv.innerText = numberToDisplay;
 }
 
-function handleEqual(total){
-    // console.log(total);
-    // displayDiv.innerText = total
+function saveValue() {
+  const prepareToCount = parseInt(inputNumber.join(""), 10);
+  passToCount.push(prepareToCount);
+  inputNumber.length = 0;
 }
 
+function count() {
+  const operatorOptions = {
+    "+": function (a, b) {
+      return a + b;
+    },
+    "-": function (a, b) {
+      return a - b;
+    },
+    "*": function (a, b) {
+      return a * b;
+    },
+    "/": function (a, b) {
+      return a / b;
+    },
+  };
 
-opeButtons.forEach((button)=>button.addEventListener('click', handleOperation));
-numButtons.forEach((button)=> button.addEventListener('click', handleNumbers));
-eqButton.addEventListener('click', handleEqual);
+  passToCount.reduce(function (prev, current) {
+    total = operatorOptions[lastOperator](prev, current);
+    passToCount.length = 0;
+    passToCount.push(total);
+    return total;
+  });
+  console.log(total);
+}
+
+function handleOperator(e) {
+  saveValue();
+  if (passToCount.length === 2) {
+    count();
+  }
+  lastOperator = e.target.innerText;
+  //   displayDiv.innerText = total;
+  console.log(passToCount);
+}
+
+function handleEqual() {
+  saveValue();
+  count();
+  displayDiv.innerText = total;
+  console.log(total);
+}
+
+numButtons.forEach((button) => button.addEventListener("click", displayValue));
+opeButtons.forEach((button) =>
+  button.addEventListener("click", handleOperator)
+);
+eqButton.addEventListener("click", handleEqual);
